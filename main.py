c6fc39b115f4ecf5ec5be949f5516198754a5bb7
TravisCallow <travisgcallow@gmail.com>
2025-01-16 11:19:10 -0800

    added something i forgot

--- main.py
+++ main.py
@@ -231,7 +231,7 @@ def save_data_to_json(data, directory='backup'):
 # Load data from JSON files
 def load_data_from_json(directory='backup'):
     data = {}
-    for table in ['users', 'sections', 'groups', 'channels', 'posts', 'study_buddy_users', 'user_facts', 'user_quotes']:
+    for table in ['users', 'sections', 'groups', 'channels', 'posts', 'study_buddy_users', 'user_facts', 'user_quotes', 'user_statistics']:
         with open(os.path.join(directory, f'{table}.json'), 'r') as f:
             data[table] = json.load(f)
     return data
